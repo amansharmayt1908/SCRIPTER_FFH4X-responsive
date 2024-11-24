40f0df8d3435198b94f23255d7ba8872eb882350
amansharmayt1908 <166890222+amansharmayt1908@users.noreply.github.com>
2024-11-25 00:27:59 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let a = document.querySelector("#cbox1")
-a.addEventListener("click",()=>{
-    window.location.href ='paid_sensi.html'
-});
-let b = document.querySelector("#cbox2")
-b.addEventListener("click",()=>{
-    window.location.href ='paid_optimization.html'
-});
