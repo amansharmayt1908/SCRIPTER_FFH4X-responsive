a6f18688de55b317b8ae2423374164b8ba435c62
amansharmayt1908 <166890222+amansharmayt1908@users.noreply.github.com>
2024-11-24 22:11:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let a = document.querySelector("#cbox1")
+a.addEventListener("click",()=>{
+    window.location.href ='paid_sensi.html'
+});
+let b = document.querySelector("#cbox2")
+b.addEventListener("click",()=>{
+    window.location.href ='paid_optimization.html'
+});
